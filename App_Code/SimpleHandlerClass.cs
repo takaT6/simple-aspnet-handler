41be3ea517ce0cf5677359b1aa5346c32ed858c5
takaT6 <77357587+takaT6@users.noreply.github.com>
2022-06-13 10:57:32 +0900

    Add files via upload

---
+++ App_Code/SimpleHandlerClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web;
+
+namespace SimpleHandlerNameSpace.App_Code
+{
+    public class SimpleHandlerClass : IHttpHandler
+    {
+        /// <summary>
+        /// </summary>
+        #region IHttpHandler Members
+
+        public bool IsReusable
+        {
+            get { return true; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            HttpRequest Request = context.Request;
+            HttpResponse Response = context.Response;
+
+            Response.Write("<html>");
+            Response.Write("<body>");
+            Response.Write("<h1>This is SimpleHandler.</h1>");
+            Response.Write("</body>");
+            Response.Write("</html>");
+        }
+
+        #endregion
+    }
+}
